6f1cb7e338f3905bd7cdbc87461cf78447940dcc
LiFangCoding <fanglihust@gmail.com>
2019-12-25 10:37:27 +0900

    add Thread example

---
+++ src/networkingAndThreads/MyRunnable.java
@@ -0,0 +1,17 @@
+package networkingAndThreads;
+
+public class MyRunnable implements Runnable {
+    public void run() {
+      go();
+    }
+
+    public void go() {
+      doMore();
+    }
+
+    public void doMore() {
+      System.out.println("top o' the stack");
+    }
+}
+
+
